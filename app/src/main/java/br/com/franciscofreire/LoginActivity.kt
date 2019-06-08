11acd39662529f4d32dc4506fdb9b0e38eeb80a8
Logon Aluno <logonrmlocal@fiap.com.br>
2019-06-08 12:03:48 -0300

    Tela de Login

--- app/src/main/java/br/com/franciscofreire/LoginActivity.kt
+++ app/src/main/java/br/com/franciscofreire/LoginActivity.kt
@@ -1,12 +1,43 @@
 package br.com.franciscofreire
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        if(usuarioLogado()) {
+            primeiraTelaLogada()
+        }
+    }
+
+    private fun primeiraTelaLogada() {
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
+    }
+
+    private fun usuarioLogado(): Boolean {
+        return mAuth.currentUser == null
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth.currentUser
+        updateUI(currentUser)
     }
 }
